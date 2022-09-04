25570f412a44bf660d0fe7baaf23a88a4ac0c62b
michalsadovski <michal.sadovski@gmail.com>
2022-09-04 21:58:47 +0200

    Lessons 327

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
@@ -16,13 +14,12 @@ import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {AlertComponent} from './shared/alert/alert.component';
 import {RecipesModule} from "./recipes/recipes.module";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     ErrorPageComponent,
     AuthComponent,
@@ -35,6 +32,7 @@ import {RecipesModule} from "./recipes/recipes.module";
     ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
+    ShoppingListModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
