cbef40d78c7b69267ba36f52f275682d7a02918b
michalsadovski <michal.sadovski@gmail.com>
2022-09-05 18:15:20 +0200

    Lessons 340

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyC0DAvqnXia7C9cBO55Ab46WFSnEtCy_tg'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: 'AIzaSyC0DAvqnXia7C9cBO55Ab46WFSnEtCy_tg'
 };
 
 /*
