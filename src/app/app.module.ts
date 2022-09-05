c3f4b1c98d1e0ada4a9344c173c12a2df655fa1f
michalsadovski <michal.sadovski@gmail.com>
2022-09-05 12:35:53 +0200

    Lessons 329

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,21 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {ErrorPageComponent} from "./error-page/error-page.component";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeService} from "./recipes/recipe.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    // DropdownDirective,
     ErrorPageComponent,
     AuthComponent,
-    // LoadingSpinnerComponent,
-    // AlertComponent
   ],
   imports: [
     BrowserModule,
@@ -32,9 +27,10 @@ import {SharedModule} from "./shared/shared.module";
     SharedModule,
     RecipesModule,
     ShoppingListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
