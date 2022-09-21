9ac8e1f2e05d82d1c9b18f4dd494a2b60531a699
michalsadovski <michal.sadovski@gmail.com>
2022-09-22 00:03:20 +0200

    Lessons 387

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import {StoreModule} from "@ngrx/store";
 import {reducers} from "./store/app.reducer";
 import { EffectsModule } from '@ngrx/effects';
 import {AuthEffects} from "./auth/store/auth.effects";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ import {AuthEffects} from "./auth/store/auth.effects";
     AppRoutingModule,
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot([AuthEffects]),
-    CoreModule
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
