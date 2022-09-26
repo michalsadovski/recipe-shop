526d148e3c4a3554ee2856dd9d3749ad8241bf7f
michalsadovski <michal.sadovski@gmail.com>
2022-09-26 21:51:39 +0200

    Lessons 389-392 +removing unused imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,3 @@
-// import {NgModule} from '@angular/core';
-// import {BrowserModule} from '@angular/platform-browser';
-// import {AppComponent} from './app.component';
-// import {HeaderComponent} from './header/header.component';
-// import {ReactiveFormsModule} from "@angular/forms";
-// import {ErrorPageComponent} from "./error-page/error-page.component";
-// import {HttpClientModule} from "@angular/common/http";
-// import {SharedModule} from "./shared/shared.module";
-// import {CoreModule} from "./core.module";
-// import {AppRoutingModule} from "./app-routing.module";
-// import {StoreModule} from "@ngrx/store";
-// import {reducers} from "./store/app.reducer";
-// import { EffectsModule } from '@ngrx/effects';
-// import {AuthEffects} from "./auth/store/auth.effects";
-// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-// import { environment } from '../environments/environment';
-// import { StoreRouterConnectingModule } from '@ngrx/router-store';
-// import {RecipeEffects} from "./recipes/store/recipe.effects";
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
